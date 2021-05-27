2d191a7a7a4d4427c19ae09ad25c9d0ed6e9dd75
Dana Liss <danaliss@gmail.com>
2021-05-27 15:39:51 -0400

    First commit

---
+++ src/main/java/com/danaliss/rpdrquoteservice/RpdrQuoteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.danaliss.rpdrquoteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpdrQuoteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpdrQuoteServiceApplication.class, args);
+	}
+
+}
